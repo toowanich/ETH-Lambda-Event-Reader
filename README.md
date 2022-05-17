abdd9ac76a6a94d2ace447cfee52a660572cc28f
Keatkajonjumroen <keatkw@3c22fb966d77.ant.amazon.com>
2022-05-18 02:40:51 +0700

    updated readme and code

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # ETH-Lambda-Event-Reader
+## **Setup** 
+
+Code for Lambda function that uses the getPastEvents Web3 API to pull events then pushes the event into Kinesis to be recorded to S3
+
+Steps:
+1. Create A DynamoDB Table
+2. Create a Kinesis Data Stream
+3. Deploy the contract (or use your own)
+4. Create a Lambda function with this code!
+
+Lambda Environment Variables:
+TABLE_NAME: [DynamoDB Table Name]
+
+CONTRACT: [Contract Address]
+
+STREAM_NAME: [Kinesis Data Stream name]
+
+ETH_SOCKET: [WSS Endpoint for ETH operations (Infura, Alchemy or others)]
+
+
+## **Adapting for your own usage**
+Within the code in the creation of the options object, you can adapt the filter to bring only the events you want for your needs, or extend the code to pull events from multiple contracts and use a mapping object to extend functionality further.
+
+Example: 
+``` js
+let startingBlock = 0 //Acquire from state management
+let lastBlockid = 1000 //currentBlock
+let contracts = ["contract1Address", "contract2Address"]
+let topicDecoder = {
+    'topic1': topic1Function,
+    'topic2': topic2Function
+}
+ let events = _(await Promise.all(
+    Object.keys(topicDecoder).map(() => {
+        return web3.eth.getPastLogs({
+            address: contracts[i].address,
+            fromBlock: startingBlock + 1,
+            toBlock: lastBlockId,
+            topics: [value]
+            })
+        })
+    )).flatten().value()
+
+for (const event of events) {
+    //@ts-ignore
+    let topic = event.topics[0]
+    // @ts-ignore
+    let fn = topicDecoder[topic]
+    if (fn) {
+        await fn(contracts[i], event)
+    } else {
+        console.error(`Unable to Process topic ${topic}`)
+    }
+}
+```
--- index.js
+++ index.js
@@ -1,45 +1,92 @@
-import { } from 'dotenv/config'
-import Web3 from 'web3';
-import { abi } from './abi.js';
-//create socket connection
-const conectionOptions = {
-    // Enable auto reconnection
-    reconnect: {
-      auto: true,
-      delay: 5000, // ms
-      maxAttempts: 5,
-      onTimeout: false
-    }
-  };
-const web3 = new Web3(
-    new Web3.providers.WebsocketProvider(process.env.ETH_SOCKET, conectionOptions)
-)
-const tokenContract = new web3.eth.Contract(
+var AWS = require('aws-sdk');
+const Web3 = require('web3')
+const AWSWebsocketProvider = require('aws-web3-ws-provider')
+const abi = require('./abi.js')
+var firehose = new AWS.Firehose();
+AWS.config.update({
+    region: "ap-southeast-1",
+  });
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+
+exports.handler = async function(event, context) {
+    var docParams = {
+        TableName : process.env.TABLE_NAME,
+        Key:{
+            "id": "1"
+        }
+    };
+
+  var block = await docClient.get(docParams).promise();
+  console.log(block)
+  
+  const web3 = new Web3(new AWSWebsocketProvider(process.env.ETH_SOCKET));
+  const tokenContract = new web3.eth.Contract(
     abi, process.env.CONTRACT,
     (error, result) => { if (error) console.log(error) }
   )
-
-export const watchTokenTransfers = async () => {
-    //FIXME sometimes gets stuck in running status
-    //TODO implement creation of transaction when adding money
-    // console.log(running)
-    // if (running) {
-    //   return
-    // }
-    // running = true;
-    //set fromBlock to be last block checked
-    const blockObj = {currentBlock: 0}
-    console.log(blockObj !== null ? (parseInt(blockObj.currentBlock) + 1).toString() : 0)
-    let options = {
-      filter: {
-      },
-      fromBlock: blockObj !== null ? (parseInt(blockObj.currentBlock) + 1).toString() : 0,                  //Number || "earliest" || "pending" || "latest"
-      toBlock: 'latest'
-    };
-  
-    const events = await tokenContract.getPastEvents('allEvents', options)
-    console.log(events)
-    console.log(events.length)
+  let options = {
+    filter: {
+    },
+    fromBlock: block["Item"]["block"]+1,
+    // fromBlock: 1,
+    toBlock: 'latest'
   }
-console.log("test")
-watchTokenTransfers()
+  const events = await tokenContract.getPastEvents('allEvents', options)
+  const filter = events.filter((element, index, array) => element.event !== 'Transfer')
+  const toWrite = await Promise.all(filter.map(async (event) => {
+      const {_from: address = null, royalty= 0, tokenId= 0, price=0, _to: to=""} = event.returnValues
+      let date = await web3.eth.getBlock(event.blockNumber)
+      date = new Date(date.timestamp*1000).toISOString().replace('T', ' ').substr(0, 19)
+      
+      return {
+        event: event.event,
+        address,
+        tokenId,
+        royalty,
+        price: parseInt(price),
+        to,
+        date
+      }
+    }))
+  console.log(toWrite)
+    web3.currentProvider.connection.close();
+
+  let writeString = toWrite.reduce((prev, current) => prev+JSON.stringify(current)+`\n`, "")
+  console.log(writeString)
+  if(filter.length > 0){
+    var params = {
+      DeliveryStreamName: process.env.STREAM_NAME,
+      Record: { 
+          Data: writeString
+      }
+  };
+  firehose.putRecord(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+
+      context.done();
+  });
+  }
+  if(events.length > 0){
+    var writeParams = {
+    TableName:"blocks",
+    Key:{
+        "id": "1"
+    },
+    ExpressionAttributeNames:{
+        "#b": "block"
+    },
+    UpdateExpression: "set #b = :r",
+    ExpressionAttributeValues:{
+        ":r":events[events.length -1]["blockNumber"],
+    },
+    ReturnValues:"UPDATED_NEW"
+    }
+  let x = await docClient.update(writeParams).promise();
+  console.log(x)
+  }
+  
+  
+};
+
